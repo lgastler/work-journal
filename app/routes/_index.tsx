9d2751693f7475c2ef6c157455ee24e2c4171308
Lennart Gastler <lennart@lgastler.de>
2023-04-10 22:57:37 +0200

    feat(form): setup basic form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,11 @@
-import type { V2_MetaFunction } from "@remix-run/react";
+import { redirect, type ActionArgs } from "@remix-run/node";
+import { Form, type V2_MetaFunction } from "@remix-run/react";
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  console.table(Object.fromEntries(formData));
+  return redirect("/");
+}
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
@@ -12,36 +19,58 @@ export default function Index() {
         Doings and learnings. Updated weekly.
       </p>
 
-      <div className="mt-8">
-        <ul>
-          <li>
-            <p>
-              Week of Feb 2<sup>nd</sup>, 2023
-            </p>
-
-            <div className="mt-4 space-y-4">
-              <div>
-                <p>Work:</p>
-                <ul className="ml-6 list-disc">
-                  <li>First thing</li>
-                </ul>
-              </div>
-              <div>
-                <p>Learnings:</p>
-                <ul className="ml-6 list-disc">
-                  <li>First learning</li>
-                  <li>Second learning</li>
-                </ul>
-              </div>
-              <div>
-                <p>Interesting things:</p>
-                <ul className="ml-6 list-disc">
-                  <li>Something cool!</li>
-                </ul>
-              </div>
+      <div className="my-8 border p-2">
+        <Form method="post">
+          <p className="italic">Create an entry</p>
+          <div>
+            <div className="mt-4">
+              <input type="date" name="date" className="text-gray-700" />
             </div>
-          </li>
-        </ul>
+            <div className="mt-2 space-x-6">
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="work"
+                />
+                Work
+              </label>
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="learning"
+                />
+                Learning
+              </label>
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="interesting"
+                />
+                Interesting thing
+              </label>
+            </div>
+
+            <div className="mt-2">
+              <textarea
+                name="text"
+                className="w-full text-gray-700"
+                placeholder="Write your entry"
+              />
+            </div>
+
+            <div className="mt-1 text-right">
+              <button className="bg-blue-500 px-4 py-1 font-medium text-white">
+                Save
+              </button>
+            </div>
+          </div>
+        </Form>
       </div>
     </div>
   );
