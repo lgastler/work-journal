f5c068d5f5ead170258731cd55cdef83712cab44
Lennart Gastler <lennart@lgastler.de>
2023-04-10 22:30:42 +0200

    chore(init): Initial commit

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  future: {
+    unstable_tailwind: true,
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+};
