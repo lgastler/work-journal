ddb38d9b9ccbce1fb1345535f6b53884373ac2cf
Lennart Gastler <lennart@lgastler.de>
2023-04-11 10:34:22 +0200

    feat(ui): added pending ui to entry form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,14 @@
 import { redirect, type ActionArgs } from "@remix-run/node";
-import { Form, type V2_MetaFunction } from "@remix-run/react";
+import { Form, useFetcher, type V2_MetaFunction } from "@remix-run/react";
+import { format } from "date-fns";
+import { useEffect, useRef } from "react";
 import { db } from "~/db.server";
 
 export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
   const { date, type, text } = Object.fromEntries(formData);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   if (
     typeof date !== "string" ||
@@ -14,21 +18,30 @@ export async function action({ request }: ActionArgs) {
     throw new Error("Bad request");
   }
 
-  await db.entry.create({
+  return db.entry.create({
     data: {
       date: new Date(date),
       type: type,
       text: text,
     },
   });
-  return redirect("/");
 }
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [{ title: "Work Journal - lgastler" }];
 };
 
 export default function Index() {
+  const fetcher = useFetcher();
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (fetcher.state === "idle" && textareaRef.current) {
+      textareaRef.current.value = "";
+      textareaRef.current.focus();
+    }
+  }, [fetcher.state]);
+
   return (
     <div className="mx-auto max-w-7xl p-6">
       <h1 className="text-4xl text-white">Work journal</h1>
@@ -37,52 +50,72 @@ export default function Index() {
       </p>
 
       <div className="my-8 border p-2">
-        <Form method="post">
-          <p className="italic">Create an entry</p>
-          <div>
-            <div className="mt-4">
-              <input type="date" name="date" className="text-gray-700" />
-            </div>
-            <div className="mt-2 space-x-6">
-              <label>
-                <input className="mr-1" type="radio" name="type" value="work" />
-                Work
-              </label>
-              <label>
+        <p className="italic">Create an entry</p>
+        <fetcher.Form method="post" autoComplete="off">
+          <fieldset
+            className="disabled:opacity-80"
+            disabled={fetcher.state === "submitting"}
+          >
+            <div>
+              <div className="mt-4">
                 <input
-                  className="mr-1"
-                  type="radio"
-                  name="type"
-                  value="learning"
+                  type="date"
+                  name="date"
+                  required
+                  className="text-gray-900"
+                  defaultValue={format(new Date(), "yyyy-MM-dd")}
                 />
-                Learning
-              </label>
-              <label>
-                <input
-                  className="mr-1"
-                  type="radio"
-                  name="type"
-                  value="interesting"
+              </div>
+              <div className="mt-2 space-x-6">
+                <label>
+                  <input
+                    className="mr-1"
+                    type="radio"
+                    name="type"
+                    value="work"
+                    defaultChecked
+                  />
+                  Work
+                </label>
+                <label>
+                  <input
+                    className="mr-1"
+                    type="radio"
+                    name="type"
+                    required
+                    value="learning"
+                  />
+                  Learning
+                </label>
+                <label>
+                  <input
+                    className="mr-1"
+                    type="radio"
+                    name="type"
+                    value="interesting"
+                  />
+                  Interesting thing
+                </label>
+              </div>
+
+              <div className="mt-2">
+                <textarea
+                  name="text"
+                  className="w-full text-gray-700"
+                  required
+                  placeholder="Type your entry"
+                  ref={textareaRef}
                 />
-                Interesting thing
-              </label>
-            </div>
+              </div>
 
-            <div className="mt-2">
-              <textarea
-                name="text"
-                className="w-full text-gray-700"
-                placeholder="Write your entry"
-              />
+              <div className="mt-1 text-right">
+                <button className="bg-blue-500 px-4 py-1 font-medium text-white">
+                  {fetcher.state === "submitting" ? "Saving…" : "Save"}
+                </button>
+              </div>
             </div>
-
-            <div className="mt-1 text-right">
-              <button className="bg-blue-500 px-4 py-1 font-medium text-white">
-                Save
-              </button>
-            </div>
-          </div>
-        </Form>
+          </fieldset>
+        </fetcher.Form>
       </div>
     </div>
   );
